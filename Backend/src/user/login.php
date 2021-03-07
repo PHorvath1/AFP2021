cfacfe87726ddf081e6f6500717c27ce2a302a38
HorKrisz <horvathkrisz00@gmail.com>
2021-03-07 13:07:34 +0100

    Added login function

---
+++ Backend/src/user/login.php
@@ -0,0 +1,25 @@
+<?php
+    include("../config/config.php");
+    $db = new dbObj();
+    $con = $db->getConnection();
+    $requestMethod = $_SERVER['REQUEST_METHOD'];
+    switch ($requestMethod) {
+        case 'GET':
+            login($_GET['email'], $_GET['password']);
+            break;
+        
+        default:
+            break;
+    }
+
+    function login($email, $password) {
+        global $con;
+        $query = "SELECT id FROM rental_db.users WHERE email ='".$email."' AND password='".$password."'";
+        $response = array();
+        $result = mysqli_query($con, $query);
+        while ($row=mysqli_fetch_array($result)) {
+            $response[] = $row;
+        }
+        echo json_encode($response);
+}
+?>
