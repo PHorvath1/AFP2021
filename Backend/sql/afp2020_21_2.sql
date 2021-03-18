838e707e9d4f7cbd38f1d16d2c36cd6391be26f3
PHorvath1 <hpeti291@gmail.com>
2021-03-18 10:42:47 +0100

    Added image attribute to 'cars' table in database

--- Backend/sql/afp2020_21_2.sql
+++ Backend/sql/afp2020_21_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2021. Már 04. 12:33
+-- Létrehozás ideje: 2021. Már 18. 10:38
 -- Kiszolgáló verziója: 10.4.11-MariaDB
 -- PHP verzió: 7.4.3
 
@@ -35,7 +35,8 @@ CREATE TABLE `cars` (
   `category` varchar(250) NOT NULL,
   `description` varchar(500) NOT NULL,
   `price` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL
+  `quantity` int(11) NOT NULL,
+  `image` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
