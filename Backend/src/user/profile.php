466c54f41ba66655e838dfaf2274f82b7720f157
HorKrisz <horvathkrisz00@gmail.com>
2021-03-24 12:20:38 +0100

    Fix show function gave back password in Backend

--- Backend/src/user/profile.php
+++ Backend/src/user/profile.php
@@ -15,7 +15,7 @@
 
     function show($uid) {
         global $con;
-        $query = "SELECT * FROM rental_db.users WHERE id =".$uid;
+        $query = "SELECT id,name,email,address,phone_number FROM rental_db.users WHERE id =".$uid;
         $response = array("status"=>"error");
         $result = mysqli_query($con, $query);
         while ($row=mysqli_fetch_array($result)) {
