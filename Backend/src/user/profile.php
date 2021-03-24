82345e9580b21b4eda1a39130ad3292f55ce2d81
PHorvath1 <hpeti291@gmail.com>
2021-03-24 12:37:38 +0100

    Added Change Password function to backend

--- Backend/src/user/profile.php
+++ Backend/src/user/profile.php
@@ -8,7 +8,12 @@
             $uid = $_GET['uid'];
             show($uid);
             break;
-        
+        case 'UPDATE':
+            $uid = $_GET['uid'];
+            $oldPassword = $_GET['oldPassword'];
+            $newPassword = $_GET['newPassword'];
+            changePassword($uid, $oldPassword, $newPassword);
+            break;
         default:
             break;
     }
@@ -23,5 +28,19 @@
             $response["status"] = "success";
         }
         echo json_encode($response);
-}
+    }
+
+    function changePassword($uid, $oldPassword, $newPassword){
+        global $con;
+        $query = "UPDATE users SET password='".$newPassword."' WHERE id=".$uid." AND password='".$oldPassword."'";
+        if (mysqli_query($con, $query)){
+            $response = "success";
+        }
+        else {
+            $response = "error";
+        }
+        echo json_encode($response);
+
+    }
+
 ?>
