394595165f3e958407a585f0f372fc24ca56622b
HorKrisz <horvathkrisz00@gmail.com>
2021-03-24 11:56:24 +0100

    Added function to users to get their data in backend

---
+++ Backend/src/user/profile.php
@@ -0,0 +1,27 @@
+<?php
+    include("../config/config.php");
+    $db = new dbObj();
+    $con = $db->getConnection();
+    $requestMethod = $_SERVER['REQUEST_METHOD'];
+    switch ($requestMethod) {
+        case 'GET':
+            $uid = $_GET['uid'];
+            show($uid);
+            break;
+        
+        default:
+            break;
+    }
+
+    function show($uid) {
+        global $con;
+        $query = "SELECT * FROM rental_db.users WHERE id =".$uid;
+        $response = array("status"=>"error");
+        $result = mysqli_query($con, $query);
+        while ($row=mysqli_fetch_array($result)) {
+            $response[] = $row;
+            $response["status"] = "success";
+        }
+        echo json_encode($response);
+}
+?>
