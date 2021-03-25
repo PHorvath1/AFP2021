fceeac192a4acb45982599efc29eb14bc2b1bd77
PHorvath1 <hpeti291@gmail.com>
2021-03-25 10:59:56 +0100

    Added list rented cars function to backend

--- Backend/src/cars/index.php
+++ Backend/src/cars/index.php
@@ -52,4 +52,22 @@
         }
         echo json_encode($response);
     }
+
+    function getRentedCars($uid){
+        if (empty($uid)) {
+            $response = "Jelentkezzen be a funkció használatához!";
+            echo json_encode($response);
+            return;
+        }
+        global $con;
+        $query = "SELECT * FROM rental_db.rented_cars WHERE uid=".$uid;
+        $response = array();
+        $result = mysqli_query($con, $query);
+        while ($row=mysqli_fetch_array($result)) {
+            $response[] = $row;
+        }
+        echo json_encode($response);
+
+    }
+
 ?>
