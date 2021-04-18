47ac9e258287c718906db11ba0edaad80be6302d
Bence <csattos.bence@gmail.com>
2021-04-18 14:13:21 +0200

    Added Home Fragment Functions to Frontend

--- Android/app/src/main/java/com/noname/nonamerental/Views/Fragments/HomeFragment.java
+++ Android/app/src/main/java/com/noname/nonamerental/Views/Fragments/HomeFragment.java
@@ -5,17 +5,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.navigation.NavigationView;
+import com.noname.nonamerental.Model.Car;
 import com.noname.nonamerental.R;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment  {
 
-    TextView userIdTextView;
-
+    CardView homeCard;
+    CardView myProfileCard;
+    CardView myRentalCard;
+    CardView carListCard;
 
     @Nullable
     @Override
@@ -24,11 +31,51 @@ public class HomeFragment extends Fragment {
         final View rootView = inflater.inflate(R.layout.fragment_home,container,false);
 
         //Data from MainActivity
-        int userId = getArguments().getInt("UserId");
+        final int userId = getArguments().getInt("UserId");
+
+        //Card declaration
+        myRentalCard = rootView.findViewById(R.id.home_my_rentals_card);
+        myProfileCard = rootView.findViewById(R.id.home_my_profile_card);
+        carListCard = rootView.findViewById(R.id.home_car_list_card);
 
 
+        //Click Listeners
 
+
+        carListCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    getFragmentManager().beginTransaction().replace(R.id.fragment_container, new CarListFragment(), "NewFragmentTag").commit();
+                }catch (Exception e){
+                    System.out.println(e);
+                }
+            }
+        });
+
+        myRentalCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    getFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyRentalsFragment(), "NewFragmentTag").commit();
+                }catch (Exception e){
+                    System.out.println(e);
+                }
+            }
+        });
+
+        myProfileCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    getFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyProfileFragment(), "NewFragmentTag").commit();
+                }catch (Exception e){
+                    System.out.println(e);
+                }
+            }
+        });
 
         return rootView;
     }
+
 }
--- Android/app/src/main/java/com/noname/nonamerental/Views/Fragments/MyProfileFragment.java
+++ Android/app/src/main/java/com/noname/nonamerental/Views/Fragments/MyProfileFragment.java
@@ -1,6 +1,23 @@
 package com.noname.nonamerental.Views.Fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.noname.nonamerental.R;
+
 public class MyProfileFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_myprofile,container,false);
+
+        return rootView;
+
+    }
 }
